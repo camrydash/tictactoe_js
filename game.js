c94933e78dce985a7a9ab70cf15dd655bd77580b
camrydash <danyal.isran@gmail.com>
2015-01-29 19:54:59 -0500

    Code refactoring
    Cleaned up code in 'game.js'

--- game.js
+++ game.js
@@ -5,88 +5,97 @@
 };
 */
 
+var MAX_TURNS = 9;
+var PLAYERS = ["X", "O"];
+var INITIAL_PLAYER = PLAYERS[0];
 
-var game = function() {
-	this.initialize = function () {
-		this.player = "O";
+function Game() {
+
+}
+
+Game.prototype = {
+	player : INITIAL_PLAYER,
+	turn : 0,
+	spaceArray : [],
+	isComplete : false, //Game is complete when someone wins or if all spaces are filled
+	currentPlayer : function() {
+		return this.player;
+	},
+	gameComplete : function () {
+		return this.isComplete;
+	},
+	switchPlayer : function() {
+		if(this.player === PLAYERS[0])
+			this.player = PLAYERS[1];
+		else
+			this.player = PLAYERS[0];
+	},
+	insertPosition : function(pos) {
+		//debugger;
+		if (this.spaceArray[pos])
+			throw "this position is already filled";
+		this.spaceArray[pos] = this.currentPlayer();
+		this.turn++;
+		if(!this.completeCondition()) {
+			this.switchPlayer();
+			return false;
+		}
+		return true;
+	},
+	completeCondition : function() {
+		var winningConditions = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
+		if(this.turn == MAX_TURNS) {
+			updateMessage("Game is a draw!");
+			return true;
+		} else {
+			return winningConditions.some(function(item, index, array) {
+				if(this.spaceArray[item[0] - 1] == undefined)
+					return false;		
+				//[] the below condition can be true if all indexes in 'spaceArray' are 'undefined'		
+				if((this.spaceArray[item[0] - 1] === this.spaceArray[item[1] - 1]) && (this.spaceArray[item[1] - 1] === this.spaceArray[item[2] - 1])) {
+					this.isComplete = true;
+					showCompete(item);
+					return true;
+				}
+				return false;
+			}.bind(this));
+		}		
+	},
+	resetGame : function() {
+		this.player = INITIAL_PLAYER;
 		this.turn = 0;
 		this.spaceArray = [];
 		this.isComplete = false;
-	};
-	this.initialize();
-}
-
-var g = new game();
-
-game.prototype.resetGame = function() {
-	this.initialize();
-	resetView();
-};
-
-game.prototype.currentPlayer = function () {
-	return this.player;
-};
-
-game.prototype.gameComplete = function () {
-	return this.isComplete;
-};
-
-game.prototype.winCondition = function () {
-	var winningConditions = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
-	return winningConditions.some(function(element, index, array) {
-		var player = this.spaceArray[element[0] - 1];
-		if (player == undefined)
-			return false;
-		if((this.spaceArray[element[0] - 1] == this.spaceArray[element[1] - 1]) && (this.spaceArray[element[1] - 1] == this.spaceArray[element[2] - 1])) {
-			this.isComplete = true;
-			showCompete(element);
-			//some body won
-			//debugger;
-			//console.log("you win!!!");
-			//alert(player + " has won at " + element);
-			return true;
-		}
-	}.bind(this));
-};
-
-game.prototype.insertPosition = function(pos) {
-	if (this.spaceArray[pos])
-		throw "this position is already filled";
-	this.spaceArray[pos] = (this.currentPlayer());
-	if(!this.winCondition()) {
-		this.turn++;
-		if(this.turn % 2 == 0) {
-			this.player = "O";
-		} else {
-			this.player = "X";
-		}
-		return false;
 	}
-	return true;
 };
 
-var showCompete = function(arr) {
+var g = new Game();
+
+function showCompete(arr) {
 	if(arr && arr.length) {
-		console.log("winning player: " + g.currentPlayer());
-		updateStatus("winning player: " + g.currentPlayer());
+		var winMsg = "Winner: Player " + g.currentPlayer();
+		console.log(winMsg);
+		updateMessage(winMsg);
 		arr.forEach(function(e, i, a) {
 			$("table td:eq(" + --e + ")").addClass("red");
 		});
 	}
 };
 
-var resetView = function() {
+function resetView() {
+	$("#status").empty();
 	$("table td").each(function() {
 		$(this).removeClass("red").empty();
 	});
 };
 
-var updateStatus = function(msg) {
+function updateMessage(msg) {
 	$("#status").html(msg);
 };
 
 $("#reset").click(function() {
 	g.resetGame();
+	resetView();
 });
 
 $("table td").click(function() {
